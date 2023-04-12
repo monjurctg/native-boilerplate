a8491cb616cee29d4575a0589143d9ca20253de0
monjurctg <monjurpy.06@gmail.com>
2023-04-12 19:48:57 +0600

    update

--- src/components/tags/Text.js
+++ src/components/tags/Text.js
@@ -1,9 +1,10 @@
 import {StyleSheet, Text as RnText, View} from "react-native";
 import React from "react";
 import {presset} from "./text.preset";
+import {pressetData} from "../../../utils/funtions";
 
-export default function Text({children, preset = "default", style}) {
-  const textStyle = StyleSheet.compose(presset[preset], style);
+export default function Text({children, preset = ["default"], style}) {
+  const textStyle = StyleSheet.compose(pressetData(preset), style);
   return <RnText style={textStyle}>{children}</RnText>;
 }
 
